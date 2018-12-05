809ec77b4ad225cd3912a4a9f9be8f31378a3885
Peter Heesterman <heesterman.peter@gmail.com>
2018-12-05 21:39:31 +1300

    test(ts-jest): add a test to check it out

--- src/utils/isEmpty.spec.ts
+++ src/utils/isEmpty.spec.ts
@@ -1,7 +1,21 @@
+import 'ts-jest'
 
+import { isEmpty } from './isEmpty'
 /*
 Test empty
 
   - throw if not an array.
 
 */
+
+test('empty array', () => {
+  expect(isEmpty([])).toBe(true)
+})
+
+test('array with one element', () => {
+  expect(isEmpty([1])).toBe(false)
+})
+
+test('null instead of array', () => {
+  expect(() => isEmpty(null)).toThrowError(new Error(`Can't check that null or undefined is empty`))
+})
--- src/utils/isEmpty.ts
+++ src/utils/isEmpty.ts
@@ -1,5 +1,6 @@
 
 const isEmpty = (arr: Array<any>) => {
+  if (arr == null) throw new Error(`Can't check that null or undefined is empty`)
   return arr.length === 0
 }
 
