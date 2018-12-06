dbb0981921814847378b9eaa2107cf2238927892
Peter Heesterman <heesterman.peter@gmail.com>
2018-12-06 21:34:29 +1300

    test(Tokenizer): test for tokens ( , [ ] )

--- src/parser/tokenizer/index.spec.ts
+++ src/parser/tokenizer/index.spec.ts
@@ -1,7 +1,7 @@
 import 'ts-jest/utils'
 
 import { tokenizer } from '.'
-import { BraceOpen, BraceClose, Word, Colon } from '../../types/Tokens'
+import { BraceOpen, BraceClose, Word, Colon, BraketClose, BraketOpen, Comma } from '../../types/Tokens'
 
 test('braces should be tokenized', () => {
   const json = `{}`
@@ -31,3 +31,21 @@ test('colons should be tokenized', () => {
   expect(tokens[0]).toEqual(new Colon(1, 1))
 })
 
+test('arrays should be tokenized', () => {
+  const json = `[]`
+  const { tokens, errors } = tokenizer(json)
+  
+  expect(errors.length).toBe(0)
+  expect(tokens.length).toBe(2)
+  expect(tokens[0]).toEqual(new BraketOpen(1, 1))
+  expect(tokens[1]).toEqual(new BraketClose(1, 2))
+})
+
+test('commas should be tokenized', () => {
+  const json = `  ,`
+  const { tokens, errors } = tokenizer(json)
+  
+  expect(errors.length).toBe(0)
+  expect(tokens.length).toBe(1)
+  expect(tokens[0]).toEqual(new Comma(1, 3))
+})
