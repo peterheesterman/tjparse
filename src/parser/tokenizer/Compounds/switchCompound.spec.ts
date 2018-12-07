923ad8d1bf475652dee6f523406c5c0f2636c101
Peter Heesterman <heesterman.peter@gmail.com>
2018-12-07 21:36:09 +1300

    test(switchCompound): add tests for null and word

--- src/parser/tokenizer/Compounds/switchCompound.spec.ts
+++ src/parser/tokenizer/Compounds/switchCompound.spec.ts
@@ -1,9 +1,10 @@
 
 import 'ts-jest/utils'
 
-import { Error } from '../../types/Errors'
-import { Word } from '../../../types/Tokens'
+import { Error, UnterminatedStringError } from '../../types/Errors'
+import { Word, Token, Null } from '../../../types/Tokens'
 import { switchCompound } from './switchCompound'
+import { UnexpectedLiteral } from '../../types/Errors/UnexpectedToken';
 
 test('word can be picked out as a compound token', () => {
   const compound = {
@@ -23,6 +24,58 @@ test('word can be picked out as a compound token', () => {
   expect(switchCompound(compound)).toEqual(result)
 })
 
-test.skip('braceOpen returns a BraceOpen singleton', () => {
-  // Need the error case
+test('Unterminated string error', () => {
+  const compound = {
+    element: '"',
+    input: '"fajdkljflks',
+    lineNumber: 1,
+    columnNumber: 1,
+    characterNumber: 1
+  }
+
+  const token: Token = null
+  const result =  {
+    error: new UnterminatedStringError(1, 1),
+    token,
+    tokenLength: -1
+  }
+  expect(switchCompound(compound)).toEqual(result)
+})
+
+
+test('null is fine', () => {
+  const compound = {
+    element: 'n',
+    input: 'null',
+    lineNumber: 1,
+    columnNumber: 1,
+    characterNumber: 1
+  }
+
+  const error: Error = null 
+  const result = {
+    tokenLength: 4,
+    token: new Null(1, 1),
+    error
+  }
+  expect(switchCompound(compound)).toEqual(result)
+})
+
+
+test('nult is an unexpected token', () => {
+  const compound = {
+    element: 'n',
+    input: 'nult',
+    lineNumber: 1,
+    columnNumber: 1,
+    characterNumber: 1
+  }
+
+  const token: Token = null
+  const result =  {
+    error: new UnexpectedLiteral(1, 1),
+    token,
+    tokenLength: 4
+  }
+  expect(switchCompound(compound)).toEqual(result)
 })
